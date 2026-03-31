09737168bcbadb82bb692b38a0d8c822da7f4f50
Daniele Simoncini <simoncini@codeninja.it>
2026-03-31 18:01:04 +0200

    terminata app02

---
+++ App02/Contatto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App02
+{
+    public class Contatto
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Numero { get; set; }
+        public string Email { get; set; }
+        public string ToString()
+        {
+            return $"{Nome} {Cognome}:\t{Numero}\t - \t{Email}";
+        }
+    }
+}
--- App02/Program.cs
+++ App02/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace App02
+﻿using System.Text.Json;
+
+namespace App02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Contatto> rubrica = new List<Contatto>();
+            do
+            {
+                Console.Clear();
+                string comando = Chiedi("Inserisci un comando: ");
+
+                if(comando == "EXIT")
+                    break;
+
+                switch (comando)
+                {
+                    case "ADD":
+                        rubrica.Add(
+                            new Contatto
+                            {
+                                Nome = Chiedi("Nome: "),
+                                Cognome = Chiedi("Cognome: "),
+                                Numero = Chiedi("Numero: "),
+                                Email = Chiedi("Email: ")
+                            }
+                        );
+                        break;
+
+                    case "REMOVE":
+                        int daRimuovere = int.Parse(Chiedi("Dammi il numero di quello da rimuovere: "));
+                        rubrica.RemoveAt(daRimuovere);
+                        break;
+
+                    case "SAVE":
+                        string fileName = Chiedi("Dammi il nome del file: ");
+                        File.WriteAllText(fileName, JsonSerializer.Serialize(rubrica));
+                        break;
+
+                    default:
+                        foreach(Contatto singolo in rubrica)
+                        {
+                            Console.WriteLine(singolo);
+                        }
+                        break;
+                }
+
+            } while (true);
+        }
+
+        internal static string Chiedi(string domanda)
+        {
+            Console.Write(domanda);
+            return Console.ReadLine();
         }
     }
 }
