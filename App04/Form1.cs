bb4f7c03663bee89087c757c051497713957c011
Daniele Simoncini <simoncini@codeninja.it>
2026-04-09 10:58:20 +0200

    upload modifiche di metà mattinata

--- App04/Form1.cs
+++ App04/Form1.cs
@@ -2,23 +2,37 @@ namespace App04
 {
     public partial class Form1 : Form
     {
+        private Graphics paint;
+        private Pen matita;
+        private Point? precedente;
 
         public Form1()
         {
             InitializeComponent();
             this.BackgroundImage = new Bitmap(this.Width, this.Height);
-
+            paint = Graphics.FromImage(this.BackgroundImage);
+            paint.Clear(Color.White);
+            matita = new Pen(Brushes.MediumSeaGreen, 3);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             if(e.Button == MouseButtons.Left)
             {
-                lblCoordinate.Text = $" {e.X} : {e.Y} ";
-                Bitmap sfondo = (Bitmap)this.BackgroundImage;
-                sfondo.SetPixel(e.X, e.Y, Color.Black);
+                if (precedente == null)
+                {
+                    precedente = e.Location;
+                    return;
+                }
+
+                Point attuale = e.Location;
+                paint.DrawLine(matita, precedente.Value, attuale);
+                precedente = attuale;
                 this.Invalidate();
-            }                
+            } else
+            {
+                precedente = null;
+            }            
         }
     }
 }
