1ee7327f3bec725d48265b5f4c933452993dd499
Daniele Simoncini <simoncini@codeninja.it>
2026-04-28 10:26:47 +0200

    inizio esercizio micro server

--- App05/Program.cs
+++ App05/Program.cs
@@ -22,22 +22,38 @@ namespace App05
             while (true)
             {
                 HttpListenerContext chiamata = serverWeb.GetContext();
-                Console.WriteLine(chiamata);
-                if(chiamata.Request.RawUrl == "/")
+                switch (chiamata.Request.RawUrl)
                 {
-                    // qui inviamo la home page
-                    Stream flusso = chiamata.Response.OutputStream;
+                    case "/file":
+                        // basandomi su cosa so
+                        string nomeFile = "prova.txt";
+                        string cartellaPubblica = "wwwroot";
+                        string posizioneEseguibile = Environment.CurrentDirectory;
+                        // calcolo il percorso del file da spedire
+                        string percorsoFile = Path.Combine(posizioneEseguibile, cartellaPubblica, nomeFile);
+                        if (!File.Exists(percorsoFile))
+                        {
+                            chiamata.Response.StatusCode = 404;
+                            chiamata.Response.Close();
+                        } else
+                        {
+                            byte[] buffer = File.ReadAllBytes(percorsoFile);
+                            chiamata.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                            chiamata.Response.Close();
+                        }
 
-                    byte[] buffer = Encoding.UTF8.GetBytes("Benvenuto nel nostro sistema online!");
+                        break;
+                    case "/in":
+                        break;
+                    case "/out":
+                        break;
+                    default: // se non ho traguardato nessuno dei precedenti... ...404.
+                        chiamata.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        chiamata.Response.Close();
+                        break;
 
-                    flusso.Write(buffer);
-                    flusso.Close();
-                } else
-                {
-                    // altrimenti diremo all'utente 404
-                    chiamata.Response.StatusCode = 404;
-                    chiamata.Response.Close();
                 }
+                
             }
             
         }
