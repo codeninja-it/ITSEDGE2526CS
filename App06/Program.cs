ae2337ee73b2943e7e3c29f43f51e7d61b9cb08a
Daniele Simoncini <simoncini@codeninja.it>
2026-05-04 14:23:38 +0200

    inizio dell'esercizio sul perceptron

---
+++ App06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace App06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // fondo il mio riconoscitore
+            Riconoscitore riconoscitore = new Riconoscitore(3);
+            // Problema: quando esco mi porto l'ombrello?
+            // Proprietà:
+            // 1. piove?
+            // 2. le previsioni dicono che pioverà?
+            // 3. dove sto andando è all'aperto?
+            Console.WriteLine(riconoscitore);
+            // in insegnamento
+            // ora piove, le previsioni dicono pioggia e vado all'aperto
+            riconoscitore.Impara(new double[] { 1.0, 1.0, 1.0 }, 1.0);
+            Console.WriteLine(riconoscitore);
+            // gli insegno quando non piove
+            riconoscitore.Impara(new double[] { 0.0, 0.0, 0.0 }, 0.0);
+            Console.WriteLine(riconoscitore);
+            // gli insegno non piove, non pioverà e devo andare all'aperto
+            riconoscitore.Impara(new double[] { 0.0, 0.0, 1.0 }, 0.0);
+            Console.WriteLine(riconoscitore);
+
+            // gli dico che se pioverà lo porto
+            riconoscitore.Impara(new double[] { 0.0, 1.0, 1.0 }, 1.0);
+            Console.WriteLine(riconoscitore);
+            double scelta = riconoscitore.Decidi(riconoscitore.Predici(new double[] { 0.0, 0.0, 0.0 }));
+            Console.WriteLine(scelta);
+        }
+    }
+}
---
+++ App06/Riconoscitore.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App06
+{
+    public class Riconoscitore
+    {
+
+        public double[] ricordo;
+        public double pregiudizio;
+
+        public Riconoscitore(int proprietà) {
+            // quando la nostra AI nasce
+            // partiamo con una base deserta
+            ricordo = new double[proprietà];
+            pregiudizio = 0;
+            Random random = new Random();
+            // e nei primi mesi di vita impara causalmente
+            for (int i = 0; i < proprietà; i++)
+            {
+                ricordo[i] = (random.NextDouble() * 2) - 1;
+            }
+            // e sviluppa un suo pregiudizio
+            pregiudizio = (random.NextDouble() * 2) - 1;
+        }
+
+        // quando qualcuno gli chiederà
+        // cosa ne pensa di una certa situazione
+        public double Predici(double[] domanda)
+        {
+            // basandosi sul pregiudizio
+            double indizi = pregiudizio;
+            // per ogni proprietà che conosce
+            for (int i = 0; i < ricordo.Length; i++)
+            {
+                // e pesandola sui suoi ricordi
+                indizi += domanda[i] * ricordo[i];
+            }
+            // produrrà una predizione
+            return Decidi(indizi);
+        }
+
+        // la sua decisione basata sulla predizione
+        // sarà sempre binaria, o sì o no
+        public double Decidi(double valore)
+        {
+            if (valore >= 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        public void Impara(double[] domanda, double risultato, double fiducia = 0.1)
+        {
+            // quando imparo
+            // parto da quello che sò
+            double predizione = Predici(domanda);
+            // controllo se è differente da quello che mi viene detto
+            double errore = risultato - predizione;
+            // decido quanto ne sono confidente
+            double confidenza = errore * fiducia;
+            // e se è uguale
+            if(errore == 0)
+            {
+                // smetto di ascoltare
+                return;
+            } 
+            // altrimenti approfondisco il mio ricordo
+            for (int i = 0; i < ricordo.Length; i++)
+            {
+                // pesando ogni informazione sulla base di quanta fiducia gli concedo
+                ricordo[i] += domanda[i] * confidenza;
+            }
+            // spostando il mio pregiudizio sull'attuale confidenza
+            pregiudizio += confidenza;
+        }
+
+        public override string ToString()
+        {
+            return $"{pregiudizio}: [{string.Join(", ", ricordo)}]";
+        }
+    }
+}
