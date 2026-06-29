9d32edd143e28cf90220dd40623d20754bb8524f
Daniele Simoncini <simoncini@codeninja.it>
2026-06-29 13:02:28 +0200

    codice di calcolo distanza per indoor location

---
+++ Maui03/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui03/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using MauiWifiManager;
+using Microsoft.Extensions.Logging;
+
+namespace Maui03
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>();
+            builder.ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            // attivo libreria per gestire la scheda di rete
+            builder.UseMauiWifiManager();
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
