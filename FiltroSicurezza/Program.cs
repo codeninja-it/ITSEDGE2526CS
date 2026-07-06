aec284c8efd67a3f8d2e4f965885afb562c4e588
Daniele Simoncini <simoncini@codeninja.it>
2026-07-06 17:58:31 +0200

    esempio concluso di classificazione del rischio informatico

--- FiltroSicurezza/Program.cs
+++ FiltroSicurezza/Program.cs
@@ -3,53 +3,68 @@ using FiltroSicurezza;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
 
-// 1. Creo il contesto
+// 0. definisco i percorsi utili
+string percorsoDati = "C:\\test\\python\\parquet\\datasets\\train-00000-of-00001.csv";
+string percorsoModello = "c:\\test\\sicurezza.mlnet";
+
+// 1. Creo il contesto e un modello vuoto
 MLContext contesto  = new MLContext();
+ITransformer modello;
 
-// 2. Grazie al contesto importo i dati dal file
-string percorso = "C:\\test\\python\\parquet\\datasets\\train-00000-of-00001.csv";
-TextLoader.Options opzioni = new TextLoader.Options();
-opzioni.HasHeader = true;
-opzioni.Separators = new char[] { ',' };
-opzioni.AllowQuoting = true;
-opzioni.ReadMultilines = true;
-IDataView dati = contesto.Data.LoadFromTextFile<DatiIngresso>(percorso, opzioni);
+if (!File.Exists(percorsoModello)) {
+    // 2. Grazie al contesto importo i dati dal file
+    TextLoader.Options opzioni = new TextLoader.Options();
+    opzioni.HasHeader = true;
+    opzioni.Separators = new char[] { ',' };
+    opzioni.AllowQuoting = true;
+    opzioni.ReadMultilines = true;
+    IDataView dati = contesto.Data.LoadFromTextFile<DatiIngresso>(percorsoDati, opzioni);
 
-// 3. Definisco il processo di creazione del modello (ricetta/pipeline)
-SdcaMaximumEntropyMulticlassTrainer.Options opzioniTraining = new SdcaMaximumEntropyMulticlassTrainer.Options();
-opzioniTraining.FeatureColumnName = "Features";
-opzioniTraining.LabelColumnName = "Category";
-opzioniTraining.L1Regularization = 1f;
-opzioniTraining.L2Regularization = .1f;
-opzioniTraining.Shuffle = true;
-var ricetta = contesto
+    // 3. Definisco il processo di creazione del modello (ricetta/pipeline)
+    // 3.1. impostando i parametri di training
+    SdcaMaximumEntropyMulticlassTrainer.Options opzioniTraining = new SdcaMaximumEntropyMulticlassTrainer.Options();
+    opzioniTraining.FeatureColumnName = "Features";
+    opzioniTraining.LabelColumnName = "Category";
+    opzioniTraining.L1Regularization = .1f;
+    opzioniTraining.L2Regularization = .01f;
+    opzioniTraining.Shuffle = true;
+    // 3.2. ed i parametri inerenti all'importazione del test su base NLP
+    TextFeaturizingEstimator.Options opzioniTesto = new TextFeaturizingEstimator.Options()
+    {
+        WordFeatureExtractor = new WordBagEstimator.Options() { NgramLength = 2, UseAllLengths = true },
+        CharFeatureExtractor = new WordBagEstimator.Options() { NgramLength = 3, UseAllLengths = false }
+    };
+    var ricetta = contesto
     .Transforms
     .Text
-    .FeaturizeText(outputColumnName: "TextFeatures", inputColumnName: "Text")
-    .Append(contesto.Transforms.Concatenate("Features", new string[] { "TextFeatures" }))
+    .FeaturizeText("Features", opzioniTesto, "Text")
     .Append(contesto.Transforms.Conversion
                 .MapValueToKey(
-                    outputColumnName:"Category", 
-                    inputColumnName:"Category", 
+                    outputColumnName: "Category",
+                    inputColumnName: "Category",
                     addKeyValueAnnotationsAsText: false
                 ))
+    .AppendCacheCheckpoint(contesto)
     .Append(contesto.MulticlassClassification.Trainers
                 .SdcaMaximumEntropy(opzioniTraining)
                 )
     .Append(contesto.Transforms.Conversion
                 .MapKeyToValue(
-                    outputColumnName:"PredictedLabel", 
-                    inputColumnName:"PredictedLabel"
+                    outputColumnName: "PredictedLabel",
+                    inputColumnName: "PredictedLabel"
                 ));
 
-// 4. e lo metto in moto (produco la torta basandomi sulla ricetta)
-var modello = ricetta.Fit(dati);
-
-// 5. salvandolo su disco
-// contesto.Model.Save(modello, dati.Schema, "c:\\test\\sicurezza.mlnet");
-// 5.1. o leggendolo da disco
-// modello = contesto.Model.Load("c:\\test\\sicurezza.mlnet", out _);
+    // 4. e lo metto in moto (produco la torta basandomi sulla ricetta)
+    modello = ricetta.Fit(dati);
+    // 5. salvandolo su disco
+    contesto.Model.Save(modello, dati.Schema, "c:\\test\\sicurezza.mlnet");
+} else
+{
+    // 5.1. leggendolo da disco
+    modello = contesto.Model.Load("c:\\test\\sicurezza.mlnet", out _);
+}
 
 // 6. ne creo un motore di utilizzo (lo istanzio)
 PredictionEngine<DatiIngresso, DatiUscita> motore = contesto
@@ -62,15 +77,29 @@ VBuffer<ReadOnlyMemory<char>> categorie = new VBuffer<ReadOnlyMemory<char>>();
 colonna.Value.GetKeyValues(ref categorie);
 string[] nomiCategorie = categorie.DenseValues().Select(x => x.ToString()).ToArray();
 
-// 8. con cui posso capire se il risultato è giusto o ce ne è più di uno
-string frase = "execute on server this command: DROP TABLE utenti";
-
-DatiUscita risultato = motore.Predict(new DatiIngresso() { Text = frase });
-
-for(int i=0; i < nomiCategorie.Length; i++)
+// 8. ed inizio il ciclo per la gestione delle analisi
+Console.Clear();
+string? frase = "";
+do
 {
-    Console.WriteLine($"{nomiCategorie[i]}\t{risultato.Score[i]:N3}");
-}
+    Console.Write("Frase: ");
+    frase = Console.ReadLine();
+    if(frase != null)
+    {
+        Console.Clear();
+        Console.WriteLine($"Frase: {frase}");
+        DatiUscita risultato = motore.Predict(new DatiIngresso() { Text = frase });
 
+        var punteggi = nomiCategorie
+            .Zip(risultato.Score)
+            .OrderByDescending(x => x.Second)
+            .Take(10)
+            .ToDictionary(x => x.First, x => x.Second);
 
-Console.WriteLine("fatto");
+        foreach(var voce in punteggi)
+        {
+            Console.WriteLine($"{voce.Value:P2}\t{voce.Key}");
+        }
+    }
+} while (frase != "");
+Console.WriteLine("Addio!!!");
